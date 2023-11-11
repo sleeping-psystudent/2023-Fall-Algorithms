7ec2f9fad7a73714e72c59b130771e682a45f450
sleeping-psystudent <b08207042@g.ntu.edu.tw>
2023-11-11 17:15:36 +0800

    building tracing table

--- b08207042_pa2/test.cpp
+++ b08207042_pa2/test.cpp
@@ -15,8 +15,8 @@ int main() {
     }
     //for(i=0;i<n;i++) cout<<data[i]<<" ";
 
-	//vector<vector<int>> table(n, vector<int>(n, -1));
 	vector<vector<int>> MIS(n, vector<int>(n, 0));
+    vector<vector<int>> table(n, vector<int>(n, 0));
 	int j, k;
 	int sub;
     for(sub=1;sub<n;sub++){
@@ -24,6 +24,7 @@ int main() {
         j=i+sub;
         while(j<n){
             k=data[j];
+            table[i][j]=table[i][j-1];
         	//cout<<i<<","<<j<<","<<k<<" ";
             //case 1: k>j>i or j>i>k
             if(k>j || i>k){
@@ -32,21 +33,29 @@ int main() {
             //case 2: j>k>i
             else if(j>k && k>i){
                 MIS[i][j]=MIS[i][k-1]+1+MIS[k+1][j-1];
+                table[i][j]=k; //Case 2的table應該不是這個
             }
             //case 3: k==i
             else if(k==i){
                 MIS[i][j]=MIS[i+1][j-1]+1;
+                table[i][j]=i;
             }
-
             i++;
             j++;
         }
         //cout<<endl;
     }
     
+    /*
     cout<<MIS[0][n-1]<<endl;
     for(j=0;j<n;j++){
         for(i=0;i<n;i++) cout<<MIS[i][j]<<" ";
+        cout<<endl;
+    }*/
+
+    cout<<table[0][n-1]<<endl;
+    for(j=0;j<n;j++){
+        for(i=0;i<n;i++) cout<<table[i][j]<<" ";
         cout<<endl;
     }
 }
